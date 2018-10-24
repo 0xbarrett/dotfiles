48c6febf95f572bd2bd19c056ae929d29e9439d0
Barrett Brown <barrett.brown@geappliances.com>
2018-10-24 13:26:08 -0400

    Add nvim init script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,7 @@
 
 ln -f .zshrc $HOME/.zshrc
 ln -f .gitconfig $HOME/.gitconfig
+
+[[ ! -d $HOME/.config ]] && mkdir $HOME/.config
+[[ ! -d $HOME/.config/nvim ]] && mkdir $HOME/.config/nvim
+ln -f ./.config/nvim/init.vim $HOME/.config/nvim/init.vim
